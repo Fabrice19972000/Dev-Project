148b9d2ec530b5e8402cab8e5c5867f270daf5e9
Fabrice19972000 <fabrice.careerpath@gmail.com>
2024-06-28 04:02:00 -0400

    first commit

---
+++ dev-project.sh
@@ -0,0 +1,7 @@
+echo "# Dev-Project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Fabrice19972000/Dev-Project.git
+git push -u origin main
